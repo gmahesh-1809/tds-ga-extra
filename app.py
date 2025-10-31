2ac0ced57eb3fe4808e796c03ef437dcaf08b1dc
gmahesh-1809 <gmahesh1809@gmail.com>
2025-10-31 19:29:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import wikipedia
+
+app = FastAPI()
+
+@app.get("/")
+async def get_wikipedia_url(topic: str):
+    try:
+        page = wikipedia.page(topic, auto_suggest=False)
+        return {"topic": topic, "url": page.url}
+    except:
+        return {"topic": topic, "url": "https://en.wikipedia.org/wiki/Main_Page"}
+
